d1fc80d1ac5ad4acef0a6c8edfb97f71c853bbec
kevinunigithub <kevingo@edu.aau.at>
2024-03-24 14:43:24 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,30 @@
+const { Pool } = require('pg');
+
+const express = require('express');
+
+const bodyParser = require('body-parser');
+
+const app = express();
+
+const ports = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, DELETE, OPTIONS'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Content-Type, Accept, X-Custom-Header, Authorization'
+  );
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+  next();
+});
+
+
+app.listen(ports, () => console.log(`Listening on port ${ports}`));
