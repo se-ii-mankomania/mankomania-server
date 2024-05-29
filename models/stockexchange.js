47633882e445afd990285e33865a7c9746734ae6
laulaobm <131969052+laulaobm@users.noreply.github.com>
2024-05-29 14:41:52 +0200

    refactored return values

--- models/stockexchange.js
+++ models/stockexchange.js
@@ -6,7 +6,10 @@ module.exports = class StockExchange{
     static async getAmountKvShares(session){
         try {
             const result = await db.query('SELECT amountkvshares FROM session where userid = $1', [session.userid]);
-            return result.rows;
+            if (result.rows.length > 0) {
+                return result.rows[0].amountkvshares;
+            }
+            return 0;
         } catch (error) {
             throw error;
         }
@@ -15,7 +18,10 @@ module.exports = class StockExchange{
     static async getAmountTShares(session){
         try {
             const result = await db.query('SELECT amounttshares FROM session where userid = $1', [session.userid]);
-            return result.rows;
+             if (result.rows.length > 0) {
+                return result.rows[0].amounttshares;
+             }
+             return 0;
         } catch (error) {
             throw error;
         }
@@ -24,7 +30,10 @@ module.exports = class StockExchange{
     static async getAmountBShares(session){
         try {
             const result = await db.query('SELECT amountbshares FROM session where userid = $1', [session.userid]);
-            return result.rows;
+            if (result.rows.length > 0) {
+                return result.rows[0].amountbshares;
+            }
+            return 0;
         } catch (error) {
             throw error;
         }
